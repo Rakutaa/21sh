d1a41cda2dcfd277c7c5f846a9a78f0ba007cf4e
Vesa Kuokka <vkuokka@student.hive.fi>
2020-05-05 14:53:24 +0300

    Removed bunch of unnessecary code.

--- srcs/input/cursor_movement_1.c
+++ srcs/input/cursor_movement_1.c
@@ -1,68 +1,42 @@
 #include "halfsh.h"
 #include "keyboard.h"
 
-static void	move_cursor_right(t_terminal *term, size_t len, size_t cols)
+static void	move_cursor_right(t_terminal *term)
 {
 	if (term->in->string[term->in->index])
-	{
-		if ((len + 1) / cols && !((len + 1) % cols))
-		{
-			tputs(tgetstr("do", NULL), 1, print_char);
-			term->in->line++;
-			tputs(tgetstr("cr", NULL), 1, print_char);
-		}
-		else
-			tputs(tgetstr("nd", NULL), 1, print_char);
 		term->in->index++;
-	}
 }
 
-static void	move_cursor_left(t_terminal *term, size_t len, size_t cols)
+static void	move_cursor_left(t_terminal *term)
 {
 	if (term->in->index > 0)
-	{
-		if (len / cols && !(len % cols))
-		{
-			tputs(tgetstr("up", NULL), 1, print_char);
-			term->in->line--;
-			while (cols--)
-				tputs(tgetstr("nd", NULL), 1, print_char);
-		}
-		else
-			tputs(tgetstr("le", NULL), 1, print_char);
 		term->in->index--;
-	}
 }
 
-static void move_cursor_right_word(t_terminal *term, size_t len)
+static void move_cursor_right_word(t_terminal *term)
 {
     while (term->in->string[term->in->index] && term->in->string[term->in->index] == ' ')
-        move_cursor_right(term, len, term->size.ws_col);
+        move_cursor_right(term);
     while (term->in->string[term->in->index] && term->in->string[term->in->index] != ' ')
-        move_cursor_right(term, len, term->size.ws_col);
+        move_cursor_right(term);
 }
 
-static void move_cursor_left_word(t_terminal *term, size_t len)
+static void move_cursor_left_word(t_terminal *term)
 {
     while (term->in->string[term->in->index - 1] && term->in->string[term->in->index - 1] == ' ')
-        move_cursor_left(term, len, term->size.ws_col);
+        move_cursor_left(term);
     while (term->in->string[term->in->index - 1] && term->in->string[term->in->index - 1] != ' ')
-        move_cursor_left(term, len, term->size.ws_col);
+        move_cursor_left(term);
 }
 
 void		cursor_movement_1(t_terminal *term, int sum)
 {
-	size_t	cols;
-	size_t	len;
-
-	len = term->in->index + ft_strlen(PROMPT);
-	cols = term->size.ws_col;
 	if (sum == LEFT)
-		move_cursor_left(term, len, cols);
+		move_cursor_left(term);
 	else if (sum == RIGHT)
-		move_cursor_right(term, len, cols);
+		move_cursor_right(term);
 	else if (sum == CTRL_RIGHT)
-        move_cursor_right_word(term, len);
+        move_cursor_right_word(term);
     else if (sum == CTRL_LEFT)
-        move_cursor_left_word(term, len);
+        move_cursor_left_word(term);
 }
