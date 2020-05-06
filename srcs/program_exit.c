b284acffa3606d1b06b81a7898a5944d75d705bd
Vesa Kuokka <vkuokka@student.hive.fi>
2020-05-06 10:02:41 +0300

    Program restores original term setting before exit.

--- srcs/program_exit.c
+++ srcs/program_exit.c
@@ -6,6 +6,7 @@ void	program_exit(t_terminal *term, int num)
 	num == 2 ? ft_putendl_fd("ft_select: TERM not set.", 2) : 0;
 	num == 3 ? ft_putendl_fd("ft_select: Could not access the termcap database.", 2) : 0;
 	num == 4 ? ft_putendl_fd("ft_select: Terminal type is not defined.", 2) : 0;
+	config_terminal(1, term);
 	if (term)
 	{
 		//ft_arrfree(term->env);
