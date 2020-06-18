62e5dfaaac3af395ab3a4a1f50c5aff385c308f1
Vesa Kuokka <vkuokka@student.hive.fi>
2020-06-18 11:55:24 +0300

    Changed signal_kill.

--- srcs/input/config_signal.c
+++ srcs/input/config_signal.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   config_signal.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vtran <vtran@student.42.fr>                +#+  +:+       +#+        */
+/*   By: vkuokka <vkuokka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/21 11:09:43 by vkuokka           #+#    #+#             */
-/*   Updated: 2020/06/17 13:26:40 by vtran            ###   ########.fr       */
+/*   Updated: 2020/06/18 11:53:42 by vkuokka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,23 +62,15 @@ static void	signal_continue(int signum)
 
 /*
 ** Clears the current command and prints out a prompt on a empty line.
-** Takes advantage of restoring cursor position saved by `sc' in
-** print_input function by using termcaps `rc' command.
 */
 
 static void	signal_kill(int signum)
 {
 	if (signum)
 	{
-		g_term->in->index = ft_strlen(g_term->in->string);
-		print_input(g_term);
-		ft_bzero(g_term->in->string, ft_strlen(g_term->in->string));
-		g_term->in->index = 0;
-		g_term->in->line = 0;
-		tputs(tgetstr("rc", NULL), 1, print_char);
+		init_input(g_term->in);
 		ft_putchar('\n');
-		ft_putstr(g_term->in->prompt);
-		tputs(tgetstr("sc", NULL), 1, print_char);
+		print_input(g_term);
 	}
 }
 
